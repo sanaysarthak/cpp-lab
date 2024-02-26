5814e39e21aba88f46cbb16accf6dc2d5196d11c
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2024-02-26 12:33:41 +0530

    Create *.cpp

---
+++ 2024-02-26/*.cpp
@@ -0,0 +1,42 @@
+// Program in C++ to demonstrate the working of function overloading.
+// Find the length of enrollment no and the length of student's name by using different overloaded functions name 'student.'
+
+#include <iostream>
+using namespace std;
+
+void student(long long eNo) {
+    cout << "\nEnrollment No : " << eNo << endl;
+    int c = 0;
+    while(eNo > 0)
+    {
+        eNo /= 10;
+        c++;
+    }
+    cout << "Length of Enrollment No = " << c << " characters." << endl;
+}
+
+void student(string sName) {
+    cout << "\nStudent Name : " << sName << endl;
+    int sLen = 0, i = 0;
+    while(sName[i] != '\0')
+    {   
+        i++;
+        if(sName[i] == (char)32)
+            continue;
+        sLen++;
+    }
+    cout << "Length of Student's Name = " << sLen << " characters." << endl;
+}
+int main() {
+    long long eNo;
+    string sName;
+    cout << "Enter Enrollment no. of Student : ";
+    cin >> eNo;
+    cout << "Enter name of Student : ";
+    cin.ignore(); // to remove the buffer
+    getline(cin, sName); // to take full name as input
+    // Calling overloaded functions
+    student(eNo);
+    student(sName);
+    return 0;
+}
